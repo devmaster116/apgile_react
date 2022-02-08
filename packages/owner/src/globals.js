17564a5ee50635ace2ac77d871611588ada0c185
devmaster116 <devmaster116@gmail.com>
2022-02-08 19:23:26 +0500

    - fixes for the global

---
+++ packages/owner/src/globals.js
@@ -0,0 +1,6 @@
+import customNav from "./config/nav";
+global.customNavGlobal = customNav;
+global.customRoutes = require("./config/routes");
+global.customModules = require("./config/modules");
+global.customLogin = false;
+global.customNavElements = false;
