48b731e9bd7794b9a1e82a9ec5e688b1d1ff1f78
devmaster116 <devmaster116@gmail.com>
2022-02-09 17:31:38 +0500

    mooman changes

--- packages/owner/src/index.js
+++ packages/owner/src/index.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-// import globals from './globals';
+import globals from './globals';
 import TheLayout from "./components/components/TheLayout";
 global.customLayout = TheLayout;
-
+console.log(globals,"globals");
 require(`@evenlogics/whf-reactadmin`);
