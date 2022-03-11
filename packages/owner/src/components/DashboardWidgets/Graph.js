61414607d99a359f00127a348cc522abd5258a1a
devmaster116 <devmaster116@gmail.com>
2022-03-11 16:20:49 +0500

    user dashboard

---
+++ packages/owner/src/components/DashboardWidgets/Graph.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {
+    CCard,
+    CCardBody, CCardSubtitle,
+    CCardTitle,
+    CCol,
+} from '@coreui/react-pro';
+import { Pie} from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, 
+    PointElement,
+    LineElement,
+  } from 'chart.js';
+import { LineChart } from './LineChart';
+import "../../style/style.css";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, ArcElement, Tooltip, Legend,
+    PointElement,
+    LineElement
+    );
+
+
+
+const Graph = ({type,title,subtitle,chartData,timeline}) => {
+
+    const components = {
+        pie: Pie,
+        line: LineChart
+    };
+    const SpecificStory = components[type];
+  return (
+    <CCol lg={4} >
+      <CCard className="height-graph">
+        <CCardBody>
+          <CCardTitle>{title}</CCardTitle>
+          <CCardSubtitle className="mb-2 text-medium-emphasis">
+             {subtitle}
+          </CCardSubtitle>
+          <SpecificStory data={chartData} timeline={timeline} />  
+        </CCardBody>
+      </CCard>
+    </CCol>
+  );
+}
+
+export default Graph
