eabada5b8d1a090d19e5c5590ad5660d5fe8335e
devmaster116 <devmaster116@gmail.com>
2022-03-20 01:19:12 +0500

    - Minor fix

--- packages/owner/src/components/Staff/Detail.js
+++ packages/owner/src/components/Staff/Detail.js
@@ -1,19 +1,12 @@
-import React, { useEffect,useState } from 'react';
+import React from 'react';
 import {Card, CardBody, CardHeader, Button} from 'reactstrap';
 import { connect } from "react-redux";
-import Block from "../DashboardWidgets/Block";
-import {CCol,CRow} from "@coreui/react-pro";
-import api from "@evenlogics/whf-api";
-import { getColor } from "@facepays/common";
-import Graph from '../DashboardWidgets/Graph';
 import "../../style/style.css";
 import Dashboard from "../Dashboard";
 
 const Detail = (props) => {
 
   let {id} = props.match.params;
-
-
 
   return (
    <div>
