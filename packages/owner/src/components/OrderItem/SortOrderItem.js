ea5929c4ee3ba899e3b868fb4bec1bf185ecae96
devmaster116 <devmaster116@gmail.com>
2023-01-25 12:57:53 +0500

    Added delete/add in Vb/OI Sort

--- packages/owner/src/components/OrderItem/SortOrderItem.js
+++ packages/owner/src/components/OrderItem/SortOrderItem.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { Card, CardBody, CardHeader,Button } from 'reactstrap';
+import { Card, CardBody, CardHeader, Button, Modal, ModalBody, ModalFooter, ModalHeader, } from 'reactstrap';
 import { CCol, CRow } from '@coreui/react-pro';
 import api from "@evenlogics/whf-api";
 import Select from 'react-select';
 import { connect } from "react-redux";
 import { sortableContainer, sortableElement, arrayMove } from 'react-sortable-hoc';
-
+import { toast } from 'react-toastify';
 
 
 function OrderItemSort(props) {
@@ -14,6 +14,8 @@ function OrderItemSort(props) {
   const [selectedOption, setSelectedOption] = useState(null)
 
   const [dataToSort, setDataToSort] = useState([])
+  const [showDeleteModal, setShowDeleteModal] = useState(false)
+  const [dataToDelete, setDataToDelete] = useState({ id: null, index: null })
 
   useEffect(() => {
     api.request("get", `/${props?.branchId}/locations?limit=1000`)
@@ -35,7 +37,7 @@ function OrderItemSort(props) {
   }, [selectedOption, props.branchId])
 
 
-  const onOptionChange = (e) => setSelectedOption(e) 
+  const onOptionChange = (e) => setSelectedOption(e)
 
   const SortableContainer = sortableContainer(({ children }) => <div className='sort-wrap' style={{
     overflowY: 'scroll',
@@ -43,28 +45,64 @@ function OrderItemSort(props) {
     gridGap: '20px',
     gridTemplateColumns: 'repeat(auto-fill, minmax(200px , 1fr))',
     margin: '20px 0',
-    cursor:'grab'
+    cursor: 'grab'
 
   }}  >{children}</div>)
 
   const SortableItem = sortableElement(({ children }) => children)
 
   const onSortEnd = ({ oldIndex, newIndex }) => setDataToSort(arrayMove(dataToSort, oldIndex, newIndex))
-  
-  const onSave=()=>{
-    const data=dataToSort
-    api.request("get", `/${props?.branchId}/virtual-buttons`,data)
-    .then(({ data }) => {
-     
-    })
-    .catch((error) => console.log(error));
+
+  const onSave = () => {
+    const data = dataToSort
+    api.request("get", `/${props?.branchId}/virtual-buttons`, data)
+      .then(({ data }) => {
+        toast.success('Sorted Successfully')
+      })
+      .catch((error) => {
+        toast.error('Something went wrong')
+        console.log(error)
+      });
+  }
+
+  const openDeleteModal = (id, index) => {
+    setDataToDelete({ id: id, index: index })
+    setShowDeleteModal(true)
+  }
+  const deleteItem = () => {
+    const { id, index } = dataToDelete
+    if (id && index) {
+      api.request("delete", `/${props?.branchId}/order-items/${id}`)
+        .then(({ data }) => {
+          setDataToSort((prev) => {
+            prev.splice(index, 1)
+            return [...prev]
+          })
+          onHideDeleteModal()
+          toast.success('Deleted Successfully')
+        })
+        .catch((error) => {
+          toast.error('Something went wrong')
+          console.log(error)
+        });
+    }
+
+  }
+
+  const onHideDeleteModal = () => {
+    setShowDeleteModal(false)
+    setDataToDelete({ id: null, index: null })
   }
 
   return (
     <Card className="animated fadeIn">
-      <CardHeader title={`Virtual Button Sort`} />
+      <CardHeader>
+        <div className='d-flex justify-content-between'>
+          Order Item Sort
+          <Button color='success' onClick={() => { props.history.push('/order-items/add') }} >Add Order Items</Button>
+        </div>
+      </CardHeader>
       <CardBody>
-
         <CRow>
           <CCol md={6}>
             <label>Select Location</label>
@@ -77,29 +115,39 @@ function OrderItemSort(props) {
               value={selectedOption}
             // isMulti
             />
-            
+
           </CCol>
           {
-            dataToSort.length>0 &&
+            dataToSort.length > 0 &&
 
             <CCol md={12}>
               <SortableContainer onSortEnd={onSortEnd} axis="xy" >
                 {dataToSort.map((data, index) => (
-                  <SortableItem key={`item-${data.id}`} index={index} value={data} >
-                    <div className='p-2 d-flex align-items-center sort-inner' style={{
-                      border: '1px solid #cbcbcb',
-                      borderRadius: '5px',
-                      backgroundColor: '#eef5ff'
-                    }}>
-                      <div className='ml-1'>  <i className={`fa ${data.icon}`}></i>{data.title}</div>
-                    </div>
-                  </SortableItem>
+                  <SortableContainer onSortEnd={onSortEnd} axis="xy" >
+                    {dataToSort.map((data, index) => (
+                      <SortableItem key={`item-${data.id}`} index={index} value={data} >
+                        <div className='p-2 d-flex align-items-center sort-inner justify-content-between' style={{ border: '1px solid #cbcbcb', borderRadius: '5px', backgroundColor: '#eef5ff' }}>
+                          <div className='ml-1'>  <i className={`fa ${data.icon} mr-2`}></i>{data.title}</div>
+                          <Button color='danger' onClick={() => { openDeleteModal(data.id, index) }} ><i className='fa fa-trash'></i></Button>
+                        </div>
+                      </SortableItem>
+                    ))}
+                  </SortableContainer>
                 ))}
               </SortableContainer>
               <Button color="primary" className="mt-2" onClick={onSave} >Save Order</Button>
             </CCol>
           }
         </CRow>
+
+        <Modal isOpen={showDeleteModal} >
+          <ModalHeader >Delete Item</ModalHeader>
+          <ModalBody>Are you sure, you want to delete this?</ModalBody>
+          <ModalFooter>
+            <Button color="danger" onClick={deleteItem}>Delete</Button>{" "}
+            <Button color="secondary" onClick={onHideDeleteModal}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
       </CardBody>
     </Card>
   )
