45870a6b0130be22b09af3d9d790d5fb63c891fa
devmaster116 <devmaster116@gmail.com>
2023-04-18 14:36:08 +0500

    area location in iot button

--- packages/owner/src/components/Buttons/Add.js
+++ packages/owner/src/components/Buttons/Add.js
@@ -1,28 +1,57 @@
-import React, {useState, useEffect} from 'react';
-import {Card, CardBody, CardHeader} from 'reactstrap';
-import {FormGenerator} from '@evenlogics/whf-form-generator';
-import {connect} from "react-redux";
-import {formPageTitle} from "@facepays/common";
-
-
+import React, { useState, useEffect } from 'react';
+import { Card, CardBody, CardHeader } from 'reactstrap';
+import { FormGenerator } from '@evenlogics/whf-form-generator';
+import { connect } from "react-redux";
+import { formPageTitle } from "@facepays/common";
+import Api from '@evenlogics/whf-api'
 const ButtonAdd = (props) => {
 
+    const [locationAndArea, setlocationAndArea] = useState({location:'',area:''})
     const [query, setQuery] = useState(false);
     // const [openQrCode, setsw] = useState(false);
-    const [fullDay,setFullDay]=useState("true")
+    const [fullDay, setFullDay] = useState("true")
     useEffect(() => {
         setQuery((prev) => !prev)
     }, [props.branchId]);
     console.log(query, "query")
-    const {id} = props.match.params;
-
+    const { id } = props.match.params;
+    const setValueForLocationAndArea=async (id,setFieldValue)=>{
+        try {
+            const { data } = await Api.request('get', `/${props.branchId}/button/info/${id}`)
+            setFieldValue('area', data.area_page.area.name)
+            setFieldValue('location', data.location.name) 
+        } catch (error) {
+            console.log(error)
+        }
+    }
     const fields = {
         page_id: {
             type: "advanceSelect",
             label: "Select Item Type",
             target: `${props.branchId}/pages?limit=1000`,
-            required:true,
+            required: true,
             col: 4,
+            callback: async (params, value, _loadOptions, setFieldValue) => {
+                await value
+                setValueForLocationAndArea(value.value,setFieldValue)
+            }
+        },
+        location: {
+            type: 'text',
+            disabled: true,
+            label: 'location',
+            readonly: true,
+            col: 4,
+            defaultValue:locationAndArea.location
+        },
+        area: {
+            type: 'text',
+            disabled: true,
+            label: 'area',
+            readonly: true,
+            col: 4,
+            defaultValue:locationAndArea.area
+
         },
         virtual_button_id: {
             type: 'advanceSelect',
@@ -38,13 +67,13 @@ const ButtonAdd = (props) => {
             label: "Time Slots",
             target: `${props.branchId}/slot-filters/virtualbutton?limit=1000`,
             // async: true,
-            multi:true,
+            multi: true,
             col: 4,
-            callback:async (e)=>{
+            callback: async (e) => {
                 await e
-                setFullDay(e.value && e.value.length?"false":"true")
+                setFullDay(e.value && e.value.length ? "false" : "true")
             },
-            required:false
+            required: false
         },
         // total_click: {
         //     type: "number",
@@ -57,7 +86,7 @@ const ButtonAdd = (props) => {
             label: "Message",
             col: 6
         },
-        
+
         status: {
             type: "switch",
             label: "Status",
@@ -68,9 +97,12 @@ const ButtonAdd = (props) => {
             col: 5,
             isDummyField: true
         },
-      
+
     };
 
+    const getInitialValues=(values)=>{
+        setlocationAndArea({location:values.location.name,area:values.area.name})
+    }
 
     return (
         <Card className="animated fadeIn xl-12 lg-12 md-12 sm-12 xs-12">
@@ -80,16 +112,16 @@ const ButtonAdd = (props) => {
             <CardBody>
                 <FormGenerator
                     targetEntity={`${props.branchId}/buttons`}
-                    // getValues={handleValue}
+                    getInitialValues={getInitialValues}
                     fields={fields}
                     targetId={id}
                     name={id ? "editForm" : ""}
                     // repeater={true}
-                    // initialValues={initialValues}
+                    // initialValues={locationAndArea}
                     redirect="buttons"
                     // debug={true}
                     // handleSameValueFields={['title', 'slug']}
-                    extraVals={{full_day:fullDay}}
+                    extraVals={{ full_day: fullDay }}
                 />
             </CardBody>
         </Card>
